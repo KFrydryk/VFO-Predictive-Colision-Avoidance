263f6f4573a74c07119796b0cf80745ed14c71bc
kyniu <frydryk.krzysztof@gmail.com>
2020-05-05 03:00:10 +0200

    push force changed to 1/dist

--- calcForceField.m
+++ calcForceField.m
@@ -2,24 +2,25 @@ function [out] = calcForceField(pos)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 global obstacles
-k = 0.03;
+k = 1;
 activationDistance = 13;
 force = 0;
 for i = 1:size(obstacles,2)
     dist = getDistFromObstacle(obstacles{1,i}, pos);
     sign = getObstacleSide(obstacles{1,i}, pos);
     if dist < activationDistance;
-        force = force + sign*(activationDistance-dist);
+        force = force + sign*1/dist;
+        %force = force + sign*(activationDistance-dist);
     end
 end
 %account for left border
 dist = getDistFromObstacle([10, pos(3), 0], pos);
 sign = getObstacleSide([10, pos(3), 0], pos);
-force = force + sign*(activationDistance-dist);
+force = force + sign*1/dist;
 %account for right border
 dist = getDistFromObstacle([30, pos(3), 0], pos);
 sign = getObstacleSide([30, pos(3), 0], pos);
-force = force + sign*(activationDistance-dist);
+force = force + sign*1/dist;
 out = k*force
 end
 
